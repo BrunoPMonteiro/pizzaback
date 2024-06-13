1b0b228cc9ade1a0a7ef5bff8a8e2c3264d55ae4
Bruno Monteiro <93095745+BrunoPMonteiro@users.noreply.github.com>
2024-06-13 20:50:16 -0300

    voltando

--- src/server.ts
+++ src/server.ts
@@ -30,4 +30,4 @@ app.use((err: Error, req:Request, res: Response, next: NextFunction) => {
   })
 })
 
-//app.listen(3333, () => console.log("Servidor Online"))
+app.listen(3333, () => console.log("Servidor Online"))
